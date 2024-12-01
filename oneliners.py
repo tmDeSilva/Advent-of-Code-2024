097826711833c477340883e7412022b6d4de4bb3
tmDeSilva <desilvathehan762@gmail.com>
2024-12-01 13:14:36 +0000

    Create oneliners.py

---
+++ oneliners.py
@@ -0,0 +1,3 @@
+#Day 1 part 1, part 2
+print(sum([abs(a-b) for a,b in zip(sorted(list(zip(*[[int(i) for i in line.strip().split()] for line in open("text.txt","r")]))[0]),sorted(list(zip(*[[int(i) for i in line.strip().split()] for line in open("text.txt","r")]))[1]))]))
+print(sum([a*sorted(list(zip(*[[int(i) for i in line.strip().split()] for line in open("text.txt","r")]))[1]).count(a) for a,b in zip(sorted(list(zip(*[[int(i) for i in line.strip().split()] for line in open("text.txt","r")]))[0]),sorted(list(zip(*[[int(i) for i in line.strip().split()] for line in open("text.txt","r")]))[1]))]))
